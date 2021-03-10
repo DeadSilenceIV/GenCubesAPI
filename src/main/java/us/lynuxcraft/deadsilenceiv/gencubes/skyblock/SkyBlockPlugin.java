e7ef745bc23fb7de8fc4ea8e35f6ce68997ab5b8
DeadSilenceIV <barreto-h2@hotmail.com>
2021-03-09 21:27:05 -0500

    SkyBlockPlugin class major changes.

--- src/main/java/us/lynuxcraft/deadsilenceiv/gencubes/skyblock/SkyBlockPlugin.java
+++ src/main/java/us/lynuxcraft/deadsilenceiv/gencubes/skyblock/SkyBlockPlugin.java
@@ -1,25 +1,23 @@
 package us.lynuxcraft.deadsilenceiv.gencubes.skyblock;
 
-import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.*;
+
 public abstract class SkyBlockPlugin implements Listener {
 
     public abstract String getName();
 
-    public abstract Island getIslandAtLocation(Location location);
+    protected void onDeleteIsland(Cancellable event,Island island,Player player){
 
-    public abstract Island getIslandFromDeleteIslandEvent(Event event);
+    }
 
-    public abstract Player getPlayerFromIslandDeleteEvent(Event event);
+    protected void onLeaveIsland(Cancellable event,Island island,Player player){
 
-    public abstract Island getIslandFromLeaveIslandEvent(Event event);
+    }
 
-    public abstract Player getPlayerFromIslandLeaveEvent(Event event);
+    protected void onKickIslandMember(Island island,OfflinePlayer offlinePlayer){
 
-    public abstract Island getIslandFromKickIslandMemberEvent(Event event);
-
-    public abstract OfflinePlayer getOfflinePlayerFromKickIslandMemberEvent(Event event);
+    }
 
 }
